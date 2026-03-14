`Fixed various issues and implemented gradient checking and batch normalization in the neural network code.`

batchnorm.py
@@ -10,7 +10,7 @@ class Layer_BatchNorm:
         self.epsilon=epsilon
         self.gamma=np.ones((1,n_features)) #scale
         self.beta=np.zeros((1,n_features)) #shift
-    def forward(self,inputs):
+    def forward(self,inputs,training=True):
         self.inputs=inputs
         self.mean=np.mean(inputs,axis=0,keepdims=True)
         self.var=np.var(inputs,axis=0,keepdims=True)
@@ -30,5 +30,5 @@ class Layer_BatchNorm:
         self.dinputs=dx_hat*std_inv+dvar*2*x_mu/N+dmean/N
         self.dgamma=np.sum(dvalues*self.x_hat,axis=0,keepdims=True)
         self.dbeta=np.sum(dvalues,axis=0,keepdims=True)
-        
+
 

dropout.py
@@ -2,8 +2,11 @@ import numpy as np
 class Layer_Dropout:
     def __init__(self,rate):
         self.rate=1-rate #eg 0.2 rate meas 20% neuron switched off
-    def forward(self,inputs):
+    def forward(self,inputs,training=True):
         self.inputs=inputs
+        if not training: #training->dropout active,inference->dropout disabled
+            self.output=inputs
+            return
         #make mask
         self.binary_mask=np.random.binomial(
             1,self.rate,size=inputs.shape

model.py
@@ -6,6 +6,11 @@ from losses import Loss_CategoricalCrossEntropy,Activation_Softmax_Loss_Categori
 class Model:
     def __init__(self):
         self.layers=[]
+        self.training=True
+    def train_mode(self):
+        self.training=True
+    def eval_mode(self):
+        self.training
 
     def add(self,layer):
         #self.layers.append(layer)
@@ -32,9 +37,8 @@ class Model:
         #softmaxx+CE
         self.loss_activation.backward(output,y)
         self.layers[-1].dinputs=self.loss_activation.dinputs
-        for layer in reversed(self.layers): #skip last layer
-            layer.backward(dinputs)
-            #dinputs=layer.dinputs
+        for layer in reversed(self.layers[:-1]):
+            layer.backward(layer.dinputs)
     
     def train(self,dataset,epochs=10000,batch_size=32):
         loader=DataLoader(dataset,batch_size=batch_size,shuffle=True)
@@ -43,9 +47,7 @@ class Model:
             for X_batch,y_batch in loader:
                 output=self.forward(X_batch)#fwd
                 loss=self.loss.calculate(output,y_batch)#loss
-                predictions=np.argmax(output,axis=1)#pred
-                if len(y_batch.shape)==2:
-                    y_batch=np.argmax(y_batch,axis=1)
+                predictions=np.argmax(output,axis=1)
                 accuracy=np.mean(predictions==y_batch)
                 self.backward(output,y_batch)#bwd
                 for layer in self.layers:#update params